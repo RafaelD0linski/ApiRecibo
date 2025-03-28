8d93390e166579e3e1c269ecbb399e71dab88394
Rafael Dolinski <rafaeldolinski14@gmail.com>
2025-03-27 21:34:51 -0300

    Adicionar arquivos de projeto.

---
+++ ApiRecibp/Controllers/ProdutoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutoController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] { "Produto 1", "Produto 2", "Produto 3" };
+    }
+}
