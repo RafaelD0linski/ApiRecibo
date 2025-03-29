b2bd2a393e834ee66811eda1014e458336c84800
Rafael Dolinski <rafaeldolinski14@gmail.com>
2025-03-29 15:15:03 -0300

    Ajuste na controller

--- ApiRecibp/Controllers/ProdutoController.cs
+++ ApiRecibp/Controllers/ProdutoController.cs
@@ -1,13 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 [Route("api/[controller]")]
 [ApiController]
 public class ProdutoController : ControllerBase
 {
+    private static List<string> produtos = new List<string>
+    {
+        "Produto 1", "Produto 2", "Produto 3"
+    };
+
+    // GET: api/produto
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-        return new string[] { "Produto 1", "Produto 2", "Produto 3" };
+        return Ok(produtos);
+    }
+
+    // GET: api/produto/1
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+        if (id < 0 || id >= produtos.Count)
+            return NotFound("Produto não encontrado.");
+
+        return Ok(produtos[id]);
+    }
+
+    // POST: api/produto
+    [HttpPost]
+    public ActionResult Post([FromBody] string produto)
+    {
+        produtos.Add(produto);
+        return CreatedAtAction(nameof(Get), new { id = produtos.Count - 1 }, produto);
+    }
+
+    // PUT: api/produto/1
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody] string novoProduto)
+    {
+        if (id < 0 || id >= produtos.Count)
+            return NotFound("Produto não encontrado.");
+
+        produtos[id] = novoProduto;
+        return NoContent();
+    }
+
+    // DELETE: api/produto/1
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        if (id < 0 || id >= produtos.Count)
+            return NotFound("Produto não encontrado.");
+
+        produtos.RemoveAt(id);
+        return NoContent();
     }
 }
